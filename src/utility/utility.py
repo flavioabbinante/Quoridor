ffaa5145fbf58e51c6f1ad1e56a825578730a400
GinoDelly <giovanniluca005@gmail.com>
2026-05-13 13:37:45 +0200

    creazione package utility e file utility.py

---
+++ src/utility/utility.py
@@ -0,0 +1,14 @@
+"""questo è un modulo di funzioni utili allo svolgimento del programma"""
+
+
+def checkInput(self, input: str):
+    """controlla che l'input sia corretto e che segua un certo ordine"""
+    pass
+
+def abbandonaPartita():
+    """permette al giocatore di abbandonare la partita in corso"""
+    pass
+
+def esciGioco():
+    """permette al giocatore di uscire dal gioco"""
+    pass
